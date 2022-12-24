a969c48c0f6ee735f5074b5653beb7c9d27b01c0
Sandeep <sandeepnandanwar92@gmail.com>
2022-12-24 19:46:20 +0530

    bug fix

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/', usersRouter);
+app.use('/api/user', indexRouter);
+app.use('/api/routes', usersRouter);
 
 require('./routes/dbconn/conn')
 // catch 404 and forward to error handler
